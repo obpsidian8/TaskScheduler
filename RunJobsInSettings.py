8c91c727ea00a13de79eeb7161198d78792f75cc
Seun Olarinre <seun@OBSIDIAN>
2020-07-23 22:04:02 -0400

    Added new files for running of task scheduler (Including example setings files)

--- RunJobsInSettings.py
+++ RunJobsInSettings.py
@@ -1,5 +1,7 @@
 from ScriptRunner import jobDetails
 from multiprocessing import Pool
+import json
+
 
 def scheduleScripts(jobDetailsObject: jobDetails):
     """
@@ -10,18 +12,22 @@ def scheduleScripts(jobDetailsObject: jobDetails):
     jobDetailsObject.scheduleJob()
     return
 
+
 def scriptRunner():
     """
     Specify the parameters of the job you want to run.
     Run this file (not this example , but the actual job details files that follows this pattern) and the scripts specified will run on the pc its set up on
     :return:
     """
-    dealPriceChecker = jobDetails(jobName='TestJobCircle',scriptName='TimeWaster.py', scriptLocation='C:/pythonprojects/AdvancedPythonTopics', startHour=11, startMinute=28, intervalSeconds=10, cmdLineArgs="-s 20", runOnStart=True)
-    gitPull = jobDetails(fullCommand="git status", runDirectory="C:\pythonscripts\PurchasingScripts\DealScriptsSettingsZim", runOnStart=True, intervalSeconds=10)
+    jobSettings = json.load(open('JobSettings.json'))
 
-    listOfJobObjects = [dealPriceChecker, gitPull]
+    listOfJobObjects = [jobDetails(setting) for setting in jobSettings]
 
-    processTotal = len(listOfJobObjects)
+    processTotal = 0
+    for job in jobSettings:
+        if job.get("Enabled"):
+            processTotal = processTotal + 1
+
     print(f"LOG INFO: Processing {processTotal} jobs")
 
     multiprocessPool = Pool(processTotal)
